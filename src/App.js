de3d98918fc162634401c9425ea3baa8fbb05a62
shaikat17 <shaikatpal56@gmail.com>
2022-10-24 13:32:15 +0600

    Another react js project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import List from "./components/list/List";
+import Alert from "./components/alert/Alert";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+    <form onSubmit={handleSubmit} className="grocery-form">
+      {alert.show && <Alert />}
+      <h3>Grocery Bud</h3>
+      <div className="form-control">
+      <input type="text" className="grocery" placeholder="e.g. eggs" onChange={(e) => setName(e.target.value)} />
+      <button type="submit" className="submit-btn">
+      { isEditing ? 'Edit' : 'Submit' }</button>
+      </div>
+    </form>
+      <div className="grocery-container">
+      <List />
+      <button className="clear-btn">Clear Items</button>
+      </div>
+    </section>
   );
 }
 
