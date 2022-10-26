0650053da943b0af2fdae07fd4ab7e1fafae1979
shaikat17 <shaikatpal56@gmail.com>
2022-10-26 12:30:06 +0600

    Insert and List section added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    if (!name) {
+
+    } else if (name && isEditing) {
+
+    } else {
+      const newItem = {id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem])
+
+      setName('')
+
+    }
   }
   return (
     <section className="section-center">
@@ -19,15 +31,16 @@ function App() {
       {alert.show && <Alert />}
       <h3>Grocery Bud</h3>
       <div className="form-control">
-      <input type="text" className="grocery" placeholder="e.g. eggs" onChange={(e) => setName(e.target.value)} />
+      <input type="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e) => setName(e.target.value)} />
       <button type="submit" className="submit-btn">
       { isEditing ? 'Edit' : 'Submit' }</button>
       </div>
     </form>
-      <div className="grocery-container">
-      <List />
-      <button className="clear-btn">Clear Items</button>
-      </div>
+
+    {list.length > 0 && <div className="grocery-container">
+    <List items={list} />
+    <button className="clear-btn">Clear Items</button>
+    </div> }
     </section>
   );
 }
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-const List = () => {
+const List = (props) => {
+  // console.log(props)
   return (
-    <div>List</div>
-  )
-}
+    <div className="grocery-list">
+      {props.items.map((item) => {
+        const { id, title } = item;
+        // console.log(title);
+        return (
+          <article className="grocery-item" key={id}>
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button className="edit-btn" type="button">
+                <FaEdit />
+              </button>
+              <button className="delete-btn" type="button">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
