4ffc4f86923cd0178613c48560a257a6a464ce58
shaikat17 <shaikatpal56@gmail.com>
2022-10-26 13:31:03 +0600

    Clear button functionality added

--- src/App.js
+++ src/App.js
@@ -10,43 +10,54 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-  const showAlert = (show=false, msg='', type='') => {
-    setAlert({show, msg, type})
-  }
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
 
+  const clearList = () => {
+    showAlert(true, "All Items are cleared.", "danger");
+    setList([])
+  };
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (!name) {
-      showAlert(true, 'Please Enter Value.', 'danger')
+      showAlert(true, "Please Enter Value.", "danger");
     } else if (name && isEditing) {
-
     } else {
-      const newItem = {id: new Date().getTime().toString(), title: name}
-      setList([...list, newItem])
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
 
-      setName('')
+      setName("");
 
-      showAlert(true, 'Item Added Successfully', 'success')
-
+      showAlert(true, "Item Added Successfully", "success");
     }
-  }
+  };
   return (
     <section className="section-center">
-    <form onSubmit={handleSubmit} className="grocery-form">
-      {alert.show && <Alert alt={alert} removeAlert={showAlert} />}
-      <h3>Grocery Bud</h3>
-      <div className="form-control">
-      <input type="text" className="grocery" placeholder="e.g. eggs" value={name} onChange={(e) => setName(e.target.value)} />
-      <button type="submit" className="submit-btn">
-      { isEditing ? 'Edit' : 'Submit' }</button>
-      </div>
-    </form>
+      <form onSubmit={handleSubmit} className="grocery-form">
+        {alert.show && <Alert alt={alert} removeAlert={showAlert} />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
 
-    {list.length > 0 && <div className="grocery-container">
-    <List items={list} />
-    <button className="clear-btn">Clear Items</button>
-    </div> }
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn" onClick={clearList}>Clear Items</button>
+        </div>
+      )}
     </section>
   );
 }
